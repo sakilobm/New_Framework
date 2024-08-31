34912f7445e21f749fa7645140c332f4fffde5f0
sakilobm <b.sakilobm@gmail.com>
2024-08-31 16:29:49 +0000

    Chnages : DB Updated

--- db/core_db.sql
+++ db/core_db.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 8.3.0 dump
+-- Adminer 4.8.1 MySQL 8.0.39-0ubuntu0.24.04.2 dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -7,29 +7,61 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+DROP TABLE IF EXISTS `ads`;
+CREATE TABLE `ads` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ads_of_content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 DROP TABLE IF EXISTS `auth`;
 CREATE TABLE `auth` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(32) NOT NULL,
-  `score` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0',
-  `phone` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0',
-  `email` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `password` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` tinytext NOT NULL,
+  `phone` varchar(10) NOT NULL,
   `blocked` int NOT NULL DEFAULT '0',
   `active` tinyint NOT NULL DEFAULT '0',
-  `role` enum('admin','user') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'user',
-  `request` enum('pending','accepted','default') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'default',
-  `payment_type` enum('default','gpay','paytm','phonepe','bhim') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'default',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `list_category` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `list_category` (`list_category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `auth` (`id`, `username`, `score`, `phone`, `email`, `password`, `blocked`, `active`, `role`, `request`, `payment_type`) VALUES
-(28,	'janusakil',	'3',	'9933338774',	'janusakil@gmail.com',	'$2y$09$njMk0LtdrczolVmWoZYrweWwBKMPWZ3g0LxpdS5MoX4I2xz9xII.2',	0,	1,	'admin',	'pending',	'gpay'),
-(30,	'sakiljanani',	'',	'',	'sakiljanani@gmail.com',	'$2y$09$EfDEARYPEh4nURlFLjwQI.rdZuxhHDmkLzfkeeSnIHN0MlupqRj72',	0,	1,	'admin',	'default',	'default'),
-(31,	'Sowbharathsb',	'',	'',	'sowbharathsb@gmail.com',	'$2y$09$qyTkX8Qlei4xMI4dZ50UuuoXvwFpjw6UJJW9btmoyqyPkG5D8vsJi',	0,	1,	'admin',	'default',	'default'),
-(32,	'thusheecopy@gmail.com',	'',	'',	'thusheecopy@gmail.com',	'$2y$09$5r.Mpru0RB7jPBYa9NxMN.JdtoQkKoa5Bf2BS.RA3DjvghorX4qkm',	0,	1,	'admin',	'default',	'default'),
-(34,	'rithik',	'0',	'9933338774',	'rithik@gmail.com',	'$2y$09$lHiPYALFyUW8OP.2H3vSTesCp/62oRsVeKRcO6R5d.VT7FJqyeJ7.',	0,	1,	'user',	'default',	'gpay');
+
+DROP TABLE IF EXISTS `info`;
+CREATE TABLE `info` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date_time` datetime DEFAULT NULL,
+  `ip` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_agent` varchar(416) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `country` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `city` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `uploaded_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `category` varchar(50) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 DROP TABLE IF EXISTS `session`;
 CREATE TABLE `session` (
@@ -44,4 +76,20 @@ CREATE TABLE `session` (
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`),
   CONSTRAINT `session_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `auth` (`id`) ON DELETE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `urls`;
+CREATE TABLE `urls` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `short_url` varchar(255) NOT NULL,
+  `long_url` text NOT NULL,
+  `alias` varchar(255) DEFAULT NULL,
+  `domain` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `short_url` (`short_url`),
+  UNIQUE KEY `alias` (`alias`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- 2024-08-31 16:29:16
